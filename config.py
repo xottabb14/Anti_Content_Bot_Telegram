235678222daa5556177a81635badce41e332ffde
Xottabb14 <vkeduardyo@yandex.ru>
2026-03-19 23:29:05 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Токен вашего бота (получить у @BotFather)
+BOT_TOKEN = "токен_бота_телеграм"
+# Список запрещенных доменов в ссылках
+BLOCKED_DOMAINS = [
+    "instagram.com",
+    "facebook.com",
+]
+
+# Список запрещенных расширений файлов
+BLOCKED_EXTENSIONS = [
+    ".epub", ".fb2", ".mobi", ".azw", ".azw3", ".djvu",
+    ".lit", ".lrf", ".lrx", ".prc", ".chm", ".oeb",
+    ".oebzip", ".tr", ".tcr", ".rb", ".pdb", ".fb3",".zip"
+]
